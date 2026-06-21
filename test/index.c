3ed9a9797db9974fa1aec2ea8f8f60b711607f3e
Uoc Azizah <uoctamika.codeberg.org@gmail.com>
2026-06-21 09:12:16 +0000

    creating test file
    Signed-off-by: Uoc Azizah <uoctamika.codeberg.org@gmail.com>

---
+++ test/index.c
@@ -0,0 +1,11 @@
+#include <libstdoc.h>
+
+int main(void)
+{
+
+   char *name = "Azizah";
+   int age = 15;
+   stdoc_printf("Hello!\n");
+   stdoc_printf("my name is %s\n", name);
+   stdoc_printf("I'm %d y/o\n", age);
+}
