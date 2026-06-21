3e75a9d61c116a0ed61419a5a873c9efc967c523
Uoc Azizah <uoctamika.codeberg.org@gmail.com>
2026-06-21 09:01:42 +0000

    ng syscall write header add check architecture
    Signed-off-by: Uoc Azizah <uoctamika.codeberg.org@gmail.com>

--- include/libstdoc/syscall/syscall_write.h
+++ include/libstdoc/syscall/syscall_write.h
@@ -3,6 +3,18 @@
 
 #include <sys/types.h>
 
+#if defined(__x86_64__)
+#define SYS_WRITE write_x86
+#elif defined(__i386__)
+#define SYS_WRITE write_i686
+#elif defined(__arm__)
+#define SYS_WRITE write_arm32
+#elif defined(__aarch64__)
+#define SYS_WRITE write_aarch64
+#else
+#error "architecture does not support or cannot being detected"
+#endif
+
 
 ssize_t write_x86(int fd, const void *buf, size_t count);
 ssize_t write_i686(int fd, const void *buf, size_t count);
